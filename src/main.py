9744843d04a878de46d672c72f2f979e3a878cb6
vovanshil95 <vovanshil95@mail.ru>
2023-08-17 10:28:28 +0300

    add user routers to main

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from fastapi.openapi.utils import get_openapi
 from auth.routes import router as auth_router
 from questions.routers import router as questions_router
 from history.routers import router as history_router
+from users.routers import router as users_router
 from config import ORIGINS
 from error_handlers import http_exception_handler
 
@@ -22,6 +23,7 @@ app.add_exception_handler(HTTPException, http_exception_handler)
 app.include_router(auth_router)
 app.include_router(questions_router)
 app.include_router(history_router)
+app.include_router(users_router)
 
 def custom_openapi():
     openapi_schema = get_openapi(
